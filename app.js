6266c5ca92f5b7d75f9c891aa3d308de6dab2d99
Harish_tkd <116169377+harish08102004@users.noreply.github.com>
2022-12-08 13:40:37 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -61,52 +61,64 @@ var items = [
   },
 ];
 function updateCart() {
-  let cart = 0;
-  for (index = 0; index < items.length; index++) {
-    cart = cart + items[index].quantity;
-  }
-  cartValue.innerHTML = cart;
-}
-
-for (let i = 0; i < addButtons.length; i++) {
-  addButtons[i].onclick = (e) => {
-    items[i].quantity++;
-    updateCart();
-  };
-}
-
-var finalDollars = 0;
-var finalCents = 0;
-
-function updatePrice() {
-  let totalPriceInCents = 0;
-
-  for (index = 0; index < items.length; index++) {
-    totalPriceInCents =
-      totalPriceInCents +
-      items[index].quantity * (items[index].dollars * 100 + items[index].cents);
-  }
-  finalDollars = Math.floor(totalPriceInCents / 100);
-  finalCents = totalPriceInCents % 100;
-}
-
-
-cartButton.onclick = () => {
-  updatePrice();
-
-
-  for (let index = 0; index < items.length; index++) {
-    if (items[index].quantity != 0) {
-      console.log(
-        "Item name: " +
-          items[index].name +
-          " - Quantity: " +
-          items[index].quantity
-      );
+    let cart = 0;
+    for (index = 0; index < items.length; index++) {
+      cart = cart + items[index].quantity;
     }
+    cartValue.innerHTML = cart;
   }
+  for (let i = 0; i < addButtons.length; i++) {
+    addButtons[i].onclick = (e) => {
+      items[i].quantity++;
+      updateCart();
+    };
+  }
+  
+  var finalDollars = 0;
+  var finalCents = 0;
+  
+  function updatePrice() {
+    let totalPriceInCents = 0;
+  
+    for (index = 0; index < items.length; index++) {
+      totalPriceInCents =
+        totalPriceInCents +
+        items[index].quantity * (items[index].dollars * 100 + items[index].cents);
+    }
+    finalDollars = Math.floor(totalPriceInCents / 100);
+    finalCents = totalPriceInCents % 100;
+  }
+  
+  var whatsappLink =
+    "https://api.whatsapp.com/send?phone=919000000000&text=Order%20details";
+  function updateWhatsappLink() {
+    for (let index = 0; index < items.length; index++) {
+      if (items[index].quantity != 0) {
+        whatsappLink += "%0A" + items[index].name + "%20" + items[index].quantity;
+      }
+    }
+    whatsappLink +=
+      "%0A" + "Total%20Price:%20$" + finalDollars + "%20" + finalCents + "c";
+  }
+  
+  cartButton.onclick = () => {
+    updatePrice();
 
-  console.log(
-    "The total amount is " + finalDollars + "$ and " + finalCents + " cents"
-  );
-};
+    updateWhatsappLink();
+    window.open(whatsappLink, "_blank");
+  
+    for (let index = 0; index < items.length; index++) {
+      if (items[index].quantity != 0) {
+        console.log(
+          "Item name: " +
+            items[index].name +
+            " - Quantity: " +
+            items[index].quantity
+        );
+      }
+    }
+  
+    console.log(
+      "The total amount is " + finalDollars + "$ and " + finalCents + " cents"
+    );
+  };
